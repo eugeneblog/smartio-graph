c58a58aa9eb169c15be652638eb1e324268681a3
eugeneblog <714284150@qq.com>
2019-03-20 11:53:47 +0800

    add meanu

--- src/components/Menus/index.js
+++ src/components/Menus/index.js
@@ -1,48 +1,80 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react'
-import {Menu, Icon} from 'antd'
+import {Menu, Dropdown} from 'antd'
 import { Logo } from "../Logo/index";
-const SubMenu = Menu.SubMenu
-const MenuItemGroup = Menu.ItemGroup
+
+const EditMenu = (
+    <Menu>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" >1st menu item</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" >2nd menu item</a>
+      </Menu.Item>
+      <Menu.Item>
+        <a target="_blank" rel="noopener noreferrer" >3rd menu item</a>
+      </Menu.Item>
+    </Menu>
+)
 
 class Menus extends Component {
-    state = {
-        current: 'mail',
+    handleMenuClick = (e) => {
+        console.log(e)
     }
-    
-    handleClick = (e) => {
-        console.log('click ', e);
-        this.setState({
-            current: e.key,
-        });
-    }
-    
     render() {
+        const fileMenu = (
+            <Menu onClick={this.handleMenuClick}>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >1st menu item</a>
+              </Menu.Item>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >2nd menu item</a>
+              </Menu.Item>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >3rd menu item</a>
+              </Menu.Item>
+            </Menu>
+        );
+        
+        const ViewMenu = (
+            <Menu>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >1st menu item</a>
+              </Menu.Item>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >2nd menu item</a>
+              </Menu.Item>
+              <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" >3rd menu item</a>
+              </Menu.Item>
+            </Menu>
+        )
         return (
             <div className="header">
                 <Logo />
                 <Menu
-                    onClick={this.handleClick}
-                    selectedKeys={[this.state.current]}
                     mode="horizontal"
                 >
-                    <SubMenu title={<span className="submenu-title-wrapper"><Icon type="folder" />File</span>}>
-                        <MenuItemGroup>
-                            <Menu.Item key="file:1">New</Menu.Item>
-                            <Menu.Item key="file:2">Open...</Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
-                    <SubMenu title={<span className="submenu-title-wrapper"><Icon type="laptop" />View</span>}>
-                        <MenuItemGroup title="Item 1">
-                            <Menu.Item key="view:1">Option 1</Menu.Item>
-                            <Menu.Item key="view:2">Option 2</Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
-                    <SubMenu title={<span className="submenu-title-wrapper"><Icon type="smile" />Help</span>}>
-                        <MenuItemGroup title="Item 1">
-                            <Menu.Item key="help:1">Option 1</Menu.Item>
-                            <Menu.Item key="help:2">Option 2</Menu.Item>
-                        </MenuItemGroup>
-                    </SubMenu>
+                    <Dropdown overlay={fileMenu}>
+                        <a className="ant-dropdown-link" href="#">
+                        File
+                        </a>
+                    </Dropdown>
+                    <Dropdown overlay={ViewMenu}>
+                        <a className="ant-dropdown-link" href="#">
+                        View
+                        </a>
+                    </Dropdown>
+                    <Dropdown overlay={EditMenu}>
+                        <a className="ant-dropdown-link" href="#">
+                        Edit
+                        </a>
+                    </Dropdown>
+                    <Dropdown overlay={EditMenu}>
+                        <a className="ant-dropdown-link" href="#">
+                        Help
+                        </a>
+                    </Dropdown>
                 </Menu>
             </div>
         )
--- src/components/Toolbar/index.js
+++ src/components/Toolbar/index.js
@@ -1,10 +1,37 @@
-import React,{ Component } from 'react'
+import { Button, Icon, Menu, Dropdown } from 'antd';
+import React,{ Component } from 'react';
+const ButtonGroup = Button.Group;
 
 class Toolbar extends Component {
     render() {
+        const menu = (
+            <Menu>
+              <Menu.Item key="1">Format Panel <span className="shortcut-text">Cmd+Shift+P</span></Menu.Item>
+              <Menu.Item key="2">Layers <span className="shortcut-text">Cmd+Shift+L</span></Menu.Item>
+              <Menu.Item key="3">Outline <span className="shortcut-text">Cmd+Shift+O</span></Menu.Item>
+            </Menu>
+        );
         return(
-            <div>
-                Toolbar
+            <div className='toolbar'>
+                <ButtonGroup>
+                    <Dropdown overlay={menu}>
+                        <Button size="small"><Icon type="layout"/><Icon type="down" /></Button>
+                    </Dropdown>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button size="small"><Icon type="zoom-in" /></Button>
+                    <Button size="small"><Icon type="zoom-out" /></Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button size="small"><Icon type="caret-left" /></Button>
+                    <Button size="small"><Icon type="caret-right" /></Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button size="small"><Icon type="delete" /></Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button size="small"><Icon type="fullscreen" /></Button>
+                </ButtonGroup>
             </div>
         )
     }
