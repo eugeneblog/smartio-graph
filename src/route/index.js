c25ed79f628b298c98e1bcf41fff062650fb33ba
eugeneblog <714284150@qq.com>
2019-03-23 14:29:55 +0800

    add user-login

--- src/route/index.js
+++ src/route/index.js
@@ -7,21 +7,15 @@ import {
 
 import HLayout from '../views/Layout'
 import Test from '../views/Test'
-import UserLogin from '../views/User/Login'
-
-const enterRoute = (v) => {
-    // 进入路由 ...
-    // 判断页面是否有用户Token，没有默认进入login
-    console.log(v)
-}
+import WrappedNormalLoginForm from '../views/User/Login'
 
 // 使用switch匹配route
 const MainRoute = () => 
     <Switch>
         <Route exact path={'/'} render={() => <Redirect to="/draw" />} />
-        <Route path={'/draw'} component={HLayout} onEnter={enterRoute.bind(this)} />
+        <Route path={'/draw'} component={HLayout} />
         <Route path={'/test'} component={Test} />
-        <Route exact path={'/user/login'} component={UserLogin}/>
+        <Route exact path={'/user/login'} component={WrappedNormalLoginForm}/>
     </Switch>
 
 export default MainRoute
--- src/views/User/Login/index.js
+++ src/views/User/Login/index.js
@@ -1,11 +1,69 @@
-import React, { Component } from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import 'antd/dist/antd.css';
+import {
+  Form, Icon, Input, Button, Checkbox,
+} from 'antd';
 
-class UserLogin extends Component {
-    render() {
-        return (
-            <div>login</div>
-        )
-    }
+// login-header
+const LoginHeader = () => {
+    return (
+        <div className="login-header">
+            <div>SmartIO-graph</div>
+            <div>describe</div>
+        </div>
+    )
 }
 
-export default UserLogin
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+        <div className="user-login">
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <LoginHeader/>
+                <Form.Item>
+                {getFieldDecorator('userName', {
+                    rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+                </Form.Item>
+                <Form.Item>
+                {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+                </Form.Item>
+                <Form.Item>
+                {getFieldDecorator('remember', {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                })(
+                    <Checkbox>Remember me</Checkbox>
+                )}
+                <a className="login-form-forgot" href="">Forgot password</a>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+
+export default WrappedNormalLoginForm
